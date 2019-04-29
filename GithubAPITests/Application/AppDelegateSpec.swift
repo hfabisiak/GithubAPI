398268f61f2d66d34cef956a370dce09ce863579
Hubert Fabisiak <hubert.fabisiak@booksy.com>
2019-04-29 13:57:40 +0200

    Add test target, add cartfile.private for test target, remove default project view controller, add initial app delegate spec and search view controller

---
+++ GithubAPITests/Application/AppDelegateSpec.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegateSpec.swift
+//  GithubAPITests
+//
+//  Created by Hubert Fabisiak on 29/04/2019.
+//  Copyright © 2019 HubertFabisiak. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+class AppDelegateSpec: QuickSpec {
+    
+    override func spec() {
+        describe("AppDelegate") {
+            
+        }
+    }
+    
+    
+}
