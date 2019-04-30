5c202f18c489f83008f2ea5a53c2650412623860
Hubert Fabisiak <hubert.fabisiak@booksy.com>
2019-04-30 15:50:48 +0200

    Add test to error message

--- GithubAPITests/Stories/Search/Model/RepoSearchErrorSpec.swift
+++ GithubAPITests/Stories/Search/Model/RepoSearchErrorSpec.swift
@@ -15,7 +15,23 @@ class RepoSearchErrorSpec: QuickSpec {
     
     override func spec() {
         describe("RepoSearchError") {
-
+            var searchErrorsResponseStub: SearchErrorsResponse!
+            var sut: RepoSearchError!
+            
+            beforeEach {
+                searchErrorsResponseStub = SearchErrorsResponse(message: "This is error message",
+                                                                errors: [])
+                sut = RepoSearchError.searchError(searchErrorsResponseStub)
+            }
+            
+            afterEach {
+                sut = nil
+                searchErrorsResponseStub = nil
+            }
+            
+            it("should have correct message") {
+                expect(sut.errorDescription) == "This is error message"
+            }
         }
     }
 
